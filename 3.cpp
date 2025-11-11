41d900d70225b9c6b49acfa44290f0e1dd2a058d
sarthak-70 <cu25720001@coeruniversity.ac.in>
2025-11-11 10:19:51 +0530

    Function to Apply Bubble Sort on an Array

---
+++ 3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
+
+int main() {
+    int arr[5] = {5, 2, 8, 1, 3};
+    bubbleSort(arr, 5);
+    cout << "Sorted array: ";
+    for (int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+    return 0;
+}
