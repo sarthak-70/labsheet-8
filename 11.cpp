4cbe677ce5941013bb13e2b943a0e9d915178040
sarthak-70 <cu25720001@coeruniversity.ac.in>
2025-11-11 10:23:44 +0530

    Class

---
+++ 11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+public:
+    string brand;
+    float price;
+
+    void input() {
+        cout << "Enter brand and price: ";
+        cin >> brand >> price;
+    }
+    void display() {
+        cout << "Brand: " << brand << ", Price: " << price << endl;
+    }
+};
+
+int main() {
+    Car c1, c2;
+    c1.input();
+    c2.input();
+    c1.display();
+    c2.display();
+    return 0;
+}
